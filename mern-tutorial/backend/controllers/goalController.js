26c2300923fbc47145a20c953ee7030595677716
yovitaa <yovitaananda89@gmail.com>
2022-07-18 01:07:45 +0700

    initial commit

--- mern-tutorial/backend/controllers/goalController.js
+++ mern-tutorial/backend/controllers/goalController.js
@@ -2,23 +2,44 @@
 const Goal = require('../models/goalModels')
 
 const getGoals = asyncHandler (async(req,res) => {
-    if(!req.body.text){
-        throw new Error('jika body data tidak ditemukan parameter text, maka akan return status 400 dan ada pesan untuk menambah parameter text')
-    }        
-
-    res.status(200).json({message: 'Get Goals'})
+    const goals = await Goal.find()
+    res.status(200).json(goals)
 })
 
 const setGoal = asyncHandler (async(req,res) => {
-    res.status(200).json({message: 'Set Goal'})
+    if(!req.body.text) {
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(goal)
 })
 
 const updateGoal = asyncHandler (async(req,res) => {
-    res.status(200).json({message: 'Update Goal ${req.params.id}'})
+    const goal = await Goal.findById(req.params.id)
+    if(!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new:true})
+    
+    res.status(200).json(updatedGoal)
 })
 
 const deleteGoal = asyncHandler (async(req,res) => {
-    res.status(200).json({message: 'Delete Goal ${req.params.id}'})
+    const goal = await Goal.findById(req.params.id)
+    if(!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    await goal.remove()
+
+    res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
