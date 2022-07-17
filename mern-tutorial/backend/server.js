f68514380e1a996c66d53a85e904d2ca6beb6e66
yovitaa <yovitaananda89@gmail.com>
2022-07-18 00:40:46 +0700

    initial commit

--- mern-tutorial/backend/server.js
+++ mern-tutorial/backend/server.js
@@ -1,9 +1,12 @@
 const asyncHandler = require ('express-async-handler')
+const colors = require('colors')
 const express = require('express')
 const dotenv = require('dotenv').config()
+const connectDb = require('./config.db')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
+connectDb()
 const app = express()
 
 app.use(express.json())
